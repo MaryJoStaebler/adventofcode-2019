e8aadf4277903caab10dc2f751698fa4c62ddf77
Mary Jo Staebler <maryjo@cosmicpixel.com>
2019-12-02 13:01:17 -0800

    Day 1 - Clean up

--- wwwroot/day1/file.js
+++ wwwroot/day1/file.js
@@ -1,15 +1,13 @@
-;
-let fileData =""
 
-function readFile(){
+function whatsTheFuel(loadDataFn){
     // read text from URL location
     let request = new XMLHttpRequest();
     request.open('GET', '/day1/data.txt');
     request.send();
-    request.onload = loadData
+    request.onload = loadDataFn
 }
 
-function loadData() {
+function calcFuelForModules() {
 
    fileModuleMass = this.response;
    arrModules = fileModuleMass.split('\n');
@@ -17,19 +15,7 @@ function loadData() {
    let modules = [];
 
    for(let i = 0; i<arrModules.length;i++){
-       let fuelRequired = 0;
-       let fuelStart = fuelCalculation(arrModules[i]);
-       fuelRequired += fuelStart;
-       console.log(fuelStart);
-       while(fuelStart > 0)
-       {
-           fuelStart = fuelCalculation(fuelStart);
-           if(fuelStart > 0){
-            fuelRequired += fuelStart;
-           }
-           console.log(fuelStart);
-       }
-       console.log("totes:"+fuelRequired);
+       let fuelRequired = fuelCalculation(arrModules[i]);
        modules.push({mass:arrModules[i],fuel:fuelRequired});
    }
 
@@ -37,12 +23,42 @@ function loadData() {
    for(let i =0; i < modules.length; i++){
        totalFuel += modules[i].fuel;
    }
-   console.log("total: "+totalFuel);
+   document.getElementById("GoalFuel1").innerHTML="Goal 1 total: "+totalFuel;
    
 }
+
+function calcFuelForModulesAdjusted() {
+
+    fileModuleMass = this.response;
+    arrModules = fileModuleMass.split('\n');
+    
+    let modules = [];
+ 
+    for(let i = 0; i<arrModules.length;i++){
+        let fuelRequired = 0;
+        let fuelStart = fuelCalculation(arrModules[i]);
+        fuelRequired += fuelStart;
+        while(fuelStart > 0)
+        {
+            fuelStart = fuelCalculation(fuelStart);
+            if(fuelStart > 0){
+             fuelRequired += fuelStart;
+            }
+        }
+        modules.push({mass:arrModules[i],fuel:fuelRequired});
+    }
+ 
+    let totalFuel = 0;
+    for(let i =0; i < modules.length; i++){
+        totalFuel += modules[i].fuel;
+    }
+    document.getElementById("GoalFuel2").innerHTML="Goal 2 total: "+totalFuel;
+    
+ }
 
 function fuelCalculation(mass){
     return Math.floor(mass/3)-2;
 }
 
-readFile();
+whatsTheFuel(calcFuelForModules);
+whatsTheFuel(calcFuelForModulesAdjusted);
